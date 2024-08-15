d49087b9787daef80087e883262f0cdb2f3a6713
Reina2024 <simmsreina@gmail.com>
2024-08-15 13:55:21 -0400

    authguard,redirect user if not logged in

---
+++ utils/authGuard.js
@@ -0,0 +1,11 @@
+// user not logged in then they are redirected
+const withAuth = (req, res, next) => {  
+    if (!req.session.loggedIn) {
+      res.redirect('/login');
+      return;
+    }
+    console.log(req.session.uid + "login")
+    next();
+  };
+  
+  module.exports = withAuth;
