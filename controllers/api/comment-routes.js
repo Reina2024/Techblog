f4a118bd905690aef7d76c1cf9148ccb89972dff
Reina2024 <simmsreina@gmail.com>
2024-08-15 17:42:47 -0400

    api routes added

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll();
+    if (!commentData) {
+      res.status(404).json({ message: 'No post!' });
+      return;
+    }
+    const comments = commentData.map((comment) => comment.toJSON());
+    console.log(comments);
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:post_id', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      where: {
+        post_id: req.params.post_id,
+      },
+      include: [{model: User, attributes: {exclude: ['password']}}]
+    });
+            
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment with this id!' });
+      return;
+    }
+
+    const comments = commentData.map((comment) => comment.toJSON());
+    console.log(comments);
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const commentData = await Comment.create({
+      content: req.body.content,
+      post_id: req.body.post_id,
+      user_id: req.session.uid,
+    });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (commentData === 0) {
+      res.status(404).json({ message: 'No comment found with that id!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ controllers/api/post-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../../models');
+
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+    if (!postData) {
+      res.status(404).json({ message: 'No post!' });
+      return;
+    }
+    const posts = postData.map((post) => post.toJSON());
+    console.log(posts);
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{model: User, attributes: {exclude: ['password']}}, {model: Comment, include:[{model: User, attributes:{exclude: ['password']} } ]}]  
+    });
+    if (!postData) {
+      res.status(404).json({ message: 'No post with this id!' });
+      return;
+    }
+    const post = postData.toJSON();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const postData = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.uid,
+    });
+    console.log(postData.toJSON());
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  console.log(req.body);
+  Post.update(
+    {
+      title: req.body.title,
+      content: req.body.content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedPost) => {
+      console.log(updatedPost);
+      if (updatedPost[0] === 0) {
+        res.status(404).json({ message: 'No post found with that id!' });
+        return;
+      }
+      res.json(updatedPost);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+module.exports = router;
