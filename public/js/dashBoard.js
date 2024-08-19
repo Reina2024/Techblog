76472d63d71a04ce7799e466bcf799dee7e9f7bc
Reina2024 <simmsreina@gmail.com>
2024-08-19 12:15:53 -0400

    fixes to help deployed site function correctly

--- public/js/dashBoard.js
+++ public/js/dashBoard.js
@@ -4,5 +4,5 @@ const addPost = document.querySelector('#add-post');
 // Add a click event listener to the 'add-post' element
 addPost.addEventListener('click', () => {
   // Redirect the user to the page where they can add a new post
-  window.location.replace(`/post`);
+  window.location.replace(`/Post`);
 });
